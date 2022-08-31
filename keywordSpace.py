ee8f31b0db95b8c147810ea470f827b1a42e756d
MinYeong <sminy98@khu.ac.kr>
2022-08-31 15:59:10 +0900

    start

--- keywordSpace.py
+++ keywordSpace.py
@@ -6,7 +6,7 @@ import folium
 longitude=127.03684   #경도 x, GetData에서 작성한 것과 같을려면 0.01씩 각각 경도 위도에 더해야 함. 여기는 중심이고 새로운 코드는 왼쪽 좌표이기 때문에
 latitude=37.49595 #위도 y
 url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
-
+#good
 #카페, (한 변이 2.25km인 사각형)
 #'radius' : 1125
 #'rect': f'{down_x},{down_y},{up_x},{up_y}'
